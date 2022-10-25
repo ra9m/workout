ce7dcb0ba08e091ec091cc6123f45b2d64629b42
ra9m <67285949+ra9m@users.noreply.github.com>
2022-10-24 21:03:26 -0700

    added some functionality, small fixes

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from workouts import program
 import sys
 import os
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton, QLabel, QLineEdit, QComboBox, QHBoxLayout
-from PyQt6.QtGui import QIcon, QIntValidator
-from PyQt6.QtCore import QSize
+import time
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
 
-class main(QMainWindow):
-    
+class startScreen(QMainWindow):
     def __init__(self):
-        super(main, self).__init__()
-        self.check = 0
+        super(startScreen, self).__init__()
         self.initialCheck()
         self.initUI()
 
@@ -26,32 +25,169 @@ class main(QMainWindow):
                 self.data = [self.name, self.squat, self.bench, self.deadlift]
         else:
             self.gotoScreen2()
+    
+    def initUI(self):
+        try:
+            self.message = QLabel(self)
+            self.message.setFixedSize(QSize(200,100))
+            self.message.setText('Welcome back, ' + self.name + '.')
+            self.message.move(350,320)
+        except AttributeError:
+            self.gotoScreen2()
+
+        # label 
+        self.title = QLabel(self)
+        self.title.setFixedSize(QSize(600,280))
+        self.title.setText('Workout\nProgram')
+        self.title.setFont(QFont('Arial', 90))
+        self.title.move(180,50)
+
+        # Fun button
+        self.fun = QPushButton(self)
+        self.fun.setFixedSize(QSize(150,50))
+        self.fun.move(100,400)
+        self.fun.setText('Mystery')
+        self.fun.clicked.connect(self.mystery)
+
+        # Main Menu button
+        self.mainMenu = QPushButton(self)
+        self.mainMenu.setFixedSize(QSize(150,50))
+        self.mainMenu.move(330,400)
+        self.mainMenu.setText('Main Menu')
+        self.mainMenu.clicked.connect(self.gotoScreen3)
+
+        # Timer button
+        self.timer = QPushButton(self)
+        self.timer.setFixedSize(QSize(150,50))
+        self.timer.move(550,400)
+        self.timer.setText('Timer')
+        self.timer.clicked.connect(self.gotoScreen4)
+    
+    def gotoScreen2(self):
+        screen2 = newPage()
+        widget.addWidget(screen2)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotoScreen3(self):
+        screen3 = mainMenu()
+        widget.addWidget(screen3)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+    
+    def gotoScreen4(self):
+        screen4 = timer()
+        widget.addWidget(screen4)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+    
+    def mystery(self):
+        pass
+
+class timer(QMainWindow):
+    def __init__(self):
+        super(timer, self).__init__()
+        self.initUI()
+    
+    def initUI(self):
+        # Title
+        self.title = QLabel(self)
+        self.title.setFixedSize(QSize(200,100))
+        self.title.setText('Timer')
+        self.title.setFont(QFont('Arial', 30))
+        self.title.move(350,20)
+
+        # Time Display
+        self.timedisplay = QLabel(self)
+        self.timedisplay.setFixedSize(QSize(200,100))
+        self.timedisplay.setText('{}{}{}')
+        self.timedisplay.setFont(QFont('Arial', 30))
+        self.timedisplay.move(350,200)
+
+        # Start button
+        self.startbutton = QPushButton(self)
+        self.startbutton.move(150,350)
+        self.startbutton.setText('Start')
+        self.startbutton.clicked.connect(self.startTimer)
+
+        # Stop button
+        self.backbutton = QPushButton(self)
+        self.backbutton.move(350,350)
+        self.backbutton.setText('Stop')
+        self.backbutton.clicked.connect(self.stopTimer)
+
+        # Reset button
+        self.backbutton = QPushButton(self)
+        self.backbutton.move(550,350)
+        self.backbutton.setText('Reset')
+        self.backbutton.clicked.connect(self.resetTimer)
+
+        # Back button
+        self.backbutton = QPushButton(self)
+        self.backbutton.move(20,20)
+        self.backbutton.setText('Back')
+        self.backbutton.clicked.connect(self.gotoHomeScreen)
+    
+    def startTimer(self):
+        self.starttime = time.time()
+
+    def stopTimer(self):
+        self.elapsedtime = time.time()
+
+    def resetTimer(self):
+        pass
+    
+    def gotoHomeScreen(self):
+        homeScreen = startScreen()
+        widget.addWidget(homeScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class mainMenu(QMainWindow):
+    
+    def __init__(self):
+        super(mainMenu, self).__init__()
+        self.check = 0
+        self.dataGrab()
+        self.initUI()
+
+    def dataGrab(self):
+        with open('prs.txt') as file:
+            lines = file.readlines()
+            data = lines[0]
+            li = list(data.split(" "))
+            self.name = li[0]
+            self.squat = li[1]
+            self.bench = li[2]
+            self.deadlift = li[3]
         
     def initUI(self):
-        # label 
-        self.label = QLabel(self)
-        self.label.setFixedSize(QSize(200,100))
+        # Back button
+        self.backbutton = QPushButton(self)
+        self.backbutton.move(20,20)
+        self.backbutton.setText('Back')
+        self.backbutton.clicked.connect(self.gotoHomeScreen)
 
-        self.label.setText('Welcome back.')
-        self.label.move(350,100)
+        # Title
+        self.title = QLabel(self)
+        self.title.setFixedSize(QSize(600,100))
+        self.title.setFont(QFont('Arial', 80))
+        self.title.setText('Main Menu')
+        self.title.move(150,170)
 
         # program button
         self.program = QPushButton(self)
         self.program.setFixedSize(QSize(150,50))
-        self.program.move(100,300)
+        self.program.move(50,350)
         self.program.setText('Program')
         self.program.clicked.connect(self.gotoScreen3)
         
         # dcp button
         self.dcp = QPushButton(self)
         self.dcp.setText('Display Current PR\'s')
-        self.dcp.move(300,300)
+        self.dcp.move(325,350)
         self.dcp.setFixedSize(QSize(150,50))
         self.dcp.clicked.connect(self.gotoScreen4)
 
         self.upr = QPushButton(self)
         self.upr.setText('Update Current PR\'s')
-        self.upr.move(500,300)
+        self.upr.move(600,350)
         self.upr.setFixedSize(QSize(150,50))
         self.upr.clicked.connect(self.gotoScreen5)
     
@@ -66,18 +202,23 @@ class main(QMainWindow):
         widget.setCurrentIndex(widget.currentIndex() + 1)
     
     def gotoScreen4(self):
-        screen4 = PR_Page()
+        screen4 = displayPRs()
         widget.addWidget(screen4)
         widget.setCurrentIndex(widget.currentIndex() + 1)
-    
+
     def gotoScreen5(self):
-        screen5 = writeNewPRs()
+        screen5 = updatePRs()
         widget.addWidget(screen5)
         widget.setCurrentIndex(widget.currentIndex() + 1)
+    
+    def gotoHomeScreen(self):
+        homeScreen = startScreen()
+        widget.addWidget(homeScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
-class writeNewPRs(QMainWindow):
+class updatePRs(QMainWindow):
     def __init__(self):
-        super(writeNewPRs, self).__init__()
+        super(updatePRs, self).__init__()
         self.arr = []
         self.initUI()
 
@@ -85,45 +226,46 @@ class writeNewPRs(QMainWindow):
         if x == 0:
             self.label2 = QLabel(self)
             self.label2.setFixedSize(QSize(200,100))
-            self.label2.setText('Enter your name:')
-            self.label2.move(350,250)
+            self.label2.setText('Enter your name and press Enter:')
+            self.label2.move(350,180)
             
         elif x == 1:
             self.label2.hide()
             self.label3 = QLabel(self)
             self.label3.setFixedSize(QSize(200,100))
-            self.label3.setText('Enter your Squat PR:')
-            self.label3.move(350,250)
+            self.label3.setText('Enter your Squat PR and press Enter:')
+            self.label3.move(350,180)
             self.label3.show()
             
         elif x == 2:
             self.label3.hide()
             self.label4 = QLabel(self)
             self.label4.setFixedSize(QSize(200,100))
-            self.label4.setText('Enter your Bench PR:')
-            self.label4.move(350,250)
+            self.label4.setText('Enter your Bench PR and press Enter:')
+            self.label4.move(350,180)
             self.label4.show()
 
         elif x == 3:
             self.label4.hide()
             self.label5 = QLabel(self)
             self.label5.setFixedSize(QSize(200,100))
-            self.label5.setText('Enter your Deadlift PR:')
-            self.label5.move(350,250)
+            self.label5.setText('Enter your Deadlift PR and press Enter:')
+            self.label5.move(350,180)
             self.label5.show()
             
     def initUI(self):
+
+        self.title = QLabel(self)
+        self.title.setFixedSize(QSize(500,50))
+        self.title.setText('Update Personal Records')
+        self.title.move(200,30)
+        self.title.setFont(QFont('Arial', 30))
+
         # Back button
         self.backbutton = QPushButton(self)
         self.backbutton.move(20,20)
         self.backbutton.setText('Back')
         self.backbutton.clicked.connect(self.gotoMainMenu)
-
-        # label for Screen 2
-        self.label = QLabel(self)
-        self.label.setFixedSize(QSize(200,100))
-        self.label.setText('Please Enter Your Current PR\'s:')
-        self.label.move(300,100)
         
         self.counter = 0
         self.display(0)
@@ -139,19 +281,18 @@ class writeNewPRs(QMainWindow):
         self.arr.append(self.temp)
         self.line.clear()
         if len(self.arr) == 4:
+            os.remove('prs.txt')
             self.keep = ' '.join(str(e) for  e in self.arr)
             f = open("prs.txt", 'a')
             f.write(self.keep)
             f.close()
-            #self.gotoMainMenu()
-            #mainWindow = main()
-            #widget.addWidget(mainWindow)
-            #widget.setCurrentIndex(widget.currentIndex()+1)
-        else:
-            print(self.arr)
+            self.gotoMainMenu()
+            mainWindow = mainMenu()
+            widget.addWidget(mainWindow)
+            widget.setCurrentIndex(widget.currentIndex()+1)
     
     def gotoMainMenu(self):
-        main_screen = main()
+        main_screen = mainMenu()
         widget.addWidget(main_screen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -169,34 +310,40 @@ class programPage(QMainWindow):
         self.backbutton.clicked.connect(self.gotoMainMenu)
 
         self.title = QLabel(self)
-        self.title.setFixedSize(QSize(500,100))
-        self.title.setText('Please Type Your Program Week #, Day #, and press \"Confirm\":')
-        self.title.move(250,100)
+        self.title.setFixedSize(QSize(400,50))
+        self.title.setText('Program')
+        self.title.move(330,30)
+        self.title.setFont(QFont('Arial', 30))
+
+        self.instructions = QLabel(self)
+        self.instructions.setFixedSize(QSize(600,100))
+        self.instructions.setText('Instructions: Enter your week and day desired, press enter to confirm, and press the \"Confirm\" button to proceed.')
+        self.instructions.move(100,50)
 
         self.weeklabel = QLabel(self)
         self.weeklabel.setFixedSize(QSize(200,100))
         self.weeklabel.setText('Week #:')
-        self.weeklabel.move(250,160)
+        self.weeklabel.move(200,110)
 
         self.daylabel = QLabel(self)
         self.daylabel.setFixedSize(QSize(200,100))
         self.daylabel.setText('Day #:')
-        self.daylabel.move(550,160)
+        self.daylabel.move(500,110)
 
         self.weekinput = QLineEdit(self)
-        self.weekinput.move(250,220)
+        self.weekinput.move(200,170)
         self.weekinput.returnPressed.connect(self.handleWeekInput)
         weekvalidator = QIntValidator()
         self.weekinput.setValidator(weekvalidator)
 
         self.dayinput = QLineEdit(self)
-        self.dayinput.move(550,220)
+        self.dayinput.move(500,170)
         self.dayinput.returnPressed.connect(self.handleDayInput)
         dayvalidator = QIntValidator()
         self.dayinput.setValidator(dayvalidator)
 
         self.readybutton = QPushButton(self)
-        self.readybutton.move(400,260)
+        self.readybutton.move(350,220)
         self.readybutton.setText('Confirm')
         self.readybutton.clicked.connect(self.accessProgram)
 
@@ -209,24 +356,27 @@ class programPage(QMainWindow):
         self.weektext = self.weekinput.text()
         self.week = QLabel(self)
         self.week.setText('Selected Week: ' + self.weektext)
-        self.week.move(250,250)
+        self.week.move(200,200)
         self.week.show()
 
     def handleDayInput(self):
         self.daytext = self.dayinput.text()
         self.day = QLabel(self)
         self.day.setText('Selected Day: ' + self.daytext)
-        self.day.move(550,250)
+        self.day.move(500,200)
         self.day.show()
 
     def accessProgram(self):
-        x = program(self.squat, self.bench, self.deadlift, self.weektext, self.daytext)
-        data_output = x.dataRetrieval()
-        self.program_output = QLabel(self)
-        self.program_output.setFixedSize(QSize(500,500))
-        self.program_output.setText(data_output)
-        self.program_output.move(200,150)
-        self.program_output.show()
+        try:
+            x = program(self.squat, self.bench, self.deadlift, self.weektext, self.daytext)
+            data_output = x.dataRetrieval()
+            self.program_output = QLabel(self)
+            self.program_output.setFixedSize(QSize(500,500))
+            self.program_output.setText(data_output)
+            self.program_output.move(200,120)
+            self.program_output.show()
+        except AttributeError:
+            pass
     
     def reset(self):
         try:
@@ -249,13 +399,13 @@ class programPage(QMainWindow):
             self.deadlift = li[3]
 
     def gotoMainMenu(self):
-        main_screen = main()
+        main_screen = mainMenu()
         widget.addWidget(main_screen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-class PR_Page(QMainWindow):
+class displayPRs(QMainWindow):
     def __init__(self):
-        super(PR_Page, self).__init__()
+        super(displayPRs, self).__init__()
         self.dataGrab()
         self.initUI()
 
@@ -265,27 +415,38 @@ class PR_Page(QMainWindow):
         self.backbutton.move(20,20)
         self.backbutton.setText('Back')
         self.backbutton.clicked.connect(self.gotoMainMenu)
+        
+        self.title = QLabel(self)
+        self.title.setFixedSize(QSize(350,50))
+        self.title.setText('Personal Records')
+        self.title.move(230,30)
+        self.title.setFont(QFont('Arial', 30))
 
         # Title
-        self.title = QLabel(self)
-        self.title.setFixedSize(QSize(200,100))
-        self.title.setText('Here are ' + self.name + '\'s current PR\'s:')
-        self.title.move(300,100)
+        self.description = QLabel(self)
+        self.description.setFixedSize(QSize(200,200))
+        self.description.setText('Here are ' + self.name + '\'s current PR\'s:\n-----------------------------')
+        self.description.move(325,100)
 
         self.squatlabel = QLabel(self)
         self.squatlabel.setFixedSize(QSize(200,100))
         self.squatlabel.setText('Squat: ' + self.squat)
-        self.squatlabel.move(350,250)
+        self.squatlabel.move(350,200)
 
         self.benchlabel = QLabel(self)
         self.benchlabel.setFixedSize(QSize(200,100))
         self.benchlabel.setText('Bench: ' + self.bench)
-        self.benchlabel.move(350,300)
+        self.benchlabel.move(350,250)
 
         self.deadliftlabel = QLabel(self)
         self.deadliftlabel.setFixedSize(QSize(200,100))
         self.deadliftlabel.setText('Deadlift: ' + self.deadlift)
-        self.deadliftlabel.move(350,350)
+        self.deadliftlabel.move(350,300)
+
+        self.updatebutton = QPushButton(self)
+        self.updatebutton.move(670,20)
+        self.updatebutton.setText('Update PR\'s')
+        self.updatebutton.clicked.connect(self.gotoUpdatePRs)
     
     def dataGrab(self):
         with open('prs.txt') as file:
@@ -298,8 +459,13 @@ class PR_Page(QMainWindow):
             self.deadlift = li[3]
             
     def gotoMainMenu(self):
-        main_screen = main()
+        main_screen = mainMenu()
         widget.addWidget(main_screen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+    
+    def gotoUpdatePRs(self):
+        updateprs = updatePRs()
+        widget.addWidget(updateprs)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 class newPage(QMainWindow):
@@ -366,19 +532,19 @@ class newPage(QMainWindow):
             f.write(self.keep)
             f.close()
             self.gotoMainMenu()
-            mainWindow = main()
+            mainWindow = mainMenu()
             widget.addWidget(mainWindow)
             widget.setCurrentIndex(widget.currentIndex()+1)
     
     def gotoMainMenu(self):
-        main_screen = main()
+        main_screen = mainMenu()
         widget.addWidget(main_screen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = QStackedWidget()
-    mainWindow = main()
+    mainWindow = startScreen()
     widget.addWidget(mainWindow)
     widget.setWindowTitle('Workout Program')
     widget.setWindowIcon(QIcon('workout.png'))
